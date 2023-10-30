ea6ed0e2fca65efb540f5341423d94c110ad66a7
Damon Allen <damon.nicholas.allen@live.com>
2023-10-30 14:40:32 -0400

    all NOW successfully passing, added tests to Spinner.test.js

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,22 @@
+import Spinner from "./Spinner"
+import React from 'react'
+import { render, fireEvent, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import '@testing-library/jest-dom/extend-expect'
+
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+
+
+test('Spinner renders', () => {
+  render(<Spinner />)
+})
+
+test('Spinner renders when on', async () => {
+  render(<Spinner on={true}/>)
+  expect(screen.getByText(/please wait.../i)).toBeInTheDocument();
 })
